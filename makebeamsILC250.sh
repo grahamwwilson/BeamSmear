4cefdc0b6f527bcdb31e2073d68b689b318fcdd4
grahamwwilson <gwwilson@ku.edu>
2022-10-02 10:24:42 +0200

    Add STATS and CORR fields to file names

--- makebeamsILC250.sh
+++ makebeamsILC250.sh
@@ -3,6 +3,10 @@
 # This is for ILC250 using the values in the file
 
 # First the electrons
+
+STATS=160k
+CORR=Plus30
+
 root -l -b -q beamsmear.C >el250.out
 
 # Sort the file by z value
@@ -16,5 +20,8 @@ root -l -b -q 'beamsmear.C("positron.ini",7,0.00152)' >pos250.out
 # Sort the file by z value
 sort -g --key=4 positron.ini >positron.tmp
 mv positron.tmp positron.ini
+
+mv electron.ini electron_${STATS}_${CORR}.ini
+mv positron.ini positron_${STATS}_${CORR}.ini
 
 exit
